8a5d2865604a8d2bf693258d31c6ea9bca2a275d
Mikhail Shabanov <dr.mboga@yahoo.com>
2021-07-07 17:14:38 +0200

    chore: WG login redirection and parameters parsing implemented

--- WotBlitzStatisticsPro.Blazor/Services/WargamingLoginService.cs
+++ WotBlitzStatisticsPro.Blazor/Services/WargamingLoginService.cs
@@ -2,6 +2,7 @@
 using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
+using Microsoft.AspNetCore.Components;
 using Microsoft.Extensions.Localization;
 using Radzen;
 using WotBlitzStatisticsPro.Blazor.Messages;
@@ -9,20 +10,26 @@ using WotBlitzStatisticsPro.Blazor.Pages;
 
 namespace WotBlitzStatisticsPro.Blazor.Services
 {
-    public class WargamingLoginService: INotificationHandler<LoginToWgMessage>, INotificationHandler<RedirectToWgLoginMessage>
+    public class WargamingLoginService: 
+        INotificationHandler<LoginToWgMessage>, 
+        INotificationHandler<RedirectToWgLoginMessage>,
+        INotificationHandler<RedirectFromWgLoginMessage>
     {
         private readonly IGraphQlBackendService _graphQlBackendService;
         private readonly DialogService _dialogService;
         private readonly IStringLocalizer<App> _localizer;
+        private readonly NavigationManager _navigationManager;
 
         public WargamingLoginService(
             IGraphQlBackendService graphQlBackendService,
             DialogService dialogService,
-            IStringLocalizer<App> localizer)
+            IStringLocalizer<App> localizer,
+            NavigationManager navigationManager)
         {
             _graphQlBackendService = graphQlBackendService;
             _dialogService = dialogService;
             _localizer = localizer;
+            _navigationManager = navigationManager;
         }
 
         public async Task Handle(LoginToWgMessage notification, CancellationToken cancellationToken)
@@ -36,8 +43,17 @@ namespace WotBlitzStatisticsPro.Blazor.Services
         public async Task Handle(RedirectToWgLoginMessage notification, CancellationToken cancellationToken)
         {
             var url = await _graphQlBackendService.GetWgLoginUrl(notification.Realm);
-            // ToDo: take a look to redirect message in previous app reincarnation
-            url = $"{url}&redirect_uri=https://developers.wargaming.net/reference/all/wot/auth/login/";
+            var baseUri = _navigationManager.BaseUri;
+            url = $"{url}&redirect_uri={baseUri}login/{notification.Realm}";
+
+            _navigationManager.NavigateTo(url);
+        }
+
+        public Task Handle(RedirectFromWgLoginMessage notification, CancellationToken cancellationToken)
+        {
+            // ToDo: Store login info to local storage
+            // ToDo: Send OpenPlayerInfoMessage with isLoggedIn = true
+            return Task.CompletedTask;
         }
     }
 }
