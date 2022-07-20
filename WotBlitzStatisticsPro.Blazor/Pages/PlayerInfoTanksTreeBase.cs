486538acc01c69d68e46b0367a63717d88288e6c
Michael Shabanov <shabanov.mikhail@gmail.com>
2022-07-20 12:19:35 +0200

    feat: tanks tree first step

---
+++ WotBlitzStatisticsPro.Blazor/Pages/PlayerInfoTanksTreeBase.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Localization;
+using System.Collections.Generic;
+using WotBlitzStatisticsPro.Blazor.GraphQl;
+
+namespace WotBlitzStatisticsPro.Blazor.Pages
+{
+    public class PlayerInfoTanksTreeBase: ComponentBase
+    {
+        [Parameter]
+        public IEnumerable<ITank> TanksList { get; set; }
+
+        [Inject]
+        public IStringLocalizer<App> Localize { get; set; }
+
+        [Inject]
+        public IMediator Mediator { get; set; }
+
+    }
+}
